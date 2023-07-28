fee59a000c1af3fb9b2b554ddf66a2571028c546
Rahul Choudhary <rahulchoudhary@walkover.in>
2023-07-28 20:00:26 +0530

    Changed project name and route `msg91` to `proxy`

--- apps/proxy/src/app/app.component.ts
+++ apps/proxy/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'msg91-root',
+    selector: 'proxy-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
