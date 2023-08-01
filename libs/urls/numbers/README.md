c60a37ce74755931265a8947b0da654c3ff3657b
Rahul Choudhary <rahulchoudhary@walkover.in>
2023-08-01 18:04:17 +0530

    Removed unnecessary URL libs

--- libs/urls/numbers/README.md
+++
@@ -1,3 +0,0 @@
-# urls-numbers
-
-This library was generated with [Nx](https://nx.dev).
--- libs/urls/security/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { createUrl } from '@msg91/service';
-
-export const AdminSecurityLogUrls = {
-    getEmailActivityLogs: (baseUrl) => createUrl(baseUrl, 'activity-logs'),
-};
