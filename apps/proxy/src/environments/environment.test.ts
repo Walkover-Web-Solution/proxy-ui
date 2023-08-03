54585e2c16d915279a355911d7d4c7fdc3526ed2
prateek-giddh <prateekmishra@giddh.com>
2023-08-03 19:07:05 +0530

    fix: Remove console log

--- apps/proxy/src/environments/environment.test.ts
+++ apps/proxy/src/environments/environment.test.ts
@@ -16,7 +16,6 @@ export const environment = {
     proxyServer: 'http://apitest.msg91.com',
     baseUrl: 'http://apitest.msg91.com/api',
 };
-console.log(environment);
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
