ef59740a81b353f9479949193f2c1b0d44acd693
Rahul Choudhary <rahulchoudhary@walkover.in>
2023-09-15 18:10:00 +0530

    Added name control and feature type control

--- apps/proxy/src/app/features/create-feature/create-feature.component.ts
+++ apps/proxy/src/app/features/create-feature/create-feature.component.ts
@@ -3,7 +3,9 @@ import { BaseComponent } from '@proxy/ui/base-component';
 import { Observable } from 'rxjs';
 import { CreateFeatureComponentStore } from './create-feature.store';
 import { IFeatureType } from '@proxy/models/features-model';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { CAMPAIGN_NAME_REGEX } from '@proxy/regex';
+import { CustomValidators } from '@proxy/custom-validator';
 
 type ServiceFormGroup = FormGroup<{
     requirements: FormGroup<{
@@ -26,9 +28,14 @@ export class CreateFeatureComponent extends BaseComponent implements OnDestroy, 
 
     public featureForm = new FormGroup({
         primaryDetails: new FormGroup({
-            name: new FormControl<string>(null),
-            feature_id: new FormControl<number>(null),
-            method_id: new FormControl<number>(null),
+            name: new FormControl<string>(null, [
+                Validators.required,
+                Validators.pattern(CAMPAIGN_NAME_REGEX),
+                CustomValidators.minLengthThreeWithoutSpace,
+                Validators.maxLength(60),
+            ]),
+            feature_id: new FormControl<number>(null, [Validators.required]),
+            method_id: new FormControl<number>(null, [Validators.required]),
         }),
         serviceDetails: new FormArray<ServiceFormGroup>([]),
     });
