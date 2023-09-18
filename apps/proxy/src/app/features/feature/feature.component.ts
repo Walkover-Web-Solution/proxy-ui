18d84d7de19b8f0f97534a5ff3a296ee7825a5b7
Rahul Choudhary <rahulchoudhary@walkover.in>
2023-09-18 21:14:14 +0530

    Added `Manage` btn in feature

--- apps/proxy/src/app/features/feature/feature.component.ts
+++ apps/proxy/src/app/features/feature/feature.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BaseComponent } from '@proxy/ui/base-component';
-import { DEFAULT_SELECTED_DATE_RANGE, SelectDateRange, PAGE_SIZE_OPTIONS } from '@proxy/constant';
-import * as dayjs from 'dayjs';
+import { PAGE_SIZE_OPTIONS } from '@proxy/constant';
 import { omit } from 'lodash';
 import { PageEvent } from '@angular/material/paginator';
 import { FeatureComponentStore } from './feature.store';
@@ -20,28 +19,16 @@ export class FeatureComponent extends BaseComponent implements OnDestroy, OnInit
     /** Store current API Inprogress State */
     public isLoading$: Observable<boolean> = this.componentStore.isLoading$;
     /** Store display column */
-    public displayedColumns: string[] = ['name', 'reference_id', 'method', 'type'];
+    public displayedColumns: string[] = ['name', 'reference_id', 'method', 'type', 'manage'];
     /** Params for fetching feature data */
     public params: any = {};
-    /** Store default selected date */
-    public selectedRangeValue = DEFAULT_SELECTED_DATE_RANGE;
-    /** Store date range */
-    public selectedDefaultDateRange = SelectDateRange;
     /** Store page size option */
     public pageSizeOptions = PAGE_SIZE_OPTIONS;
-    // public selectedDateRange = {
-    //     startDate: DEFAULT_START_DATE,
-    //     endDate: DEFAULT_END_DATE,
-    // };
 
     constructor(private componentStore: FeatureComponentStore) {
         super();
     }
     ngOnInit(): void {
-        this.params = {
-            ...this.params,
-            ...this.formatDateRange(),
-        };
         this.getFeatures();
     }
 
@@ -63,30 +50,6 @@ export class FeatureComponent extends BaseComponent implements OnDestroy, OnInit
             this.params = { ...omit(this.params, ['search']) };
         }
         this.params.pageNo = 1;
-        this.getFeatures();
-    }
-
-    /**
-     * Formate Date Range and convert into format
-     * @returns
-     */
-    private formatDateRange(): any {
-        return {
-            startDate: dayjs(this.selectedRangeValue.start).format('DD-MM-YYYY'),
-            endDate: dayjs(this.selectedRangeValue.end).format('DD-MM-YYYY'),
-        };
-    }
-
-    /**
-     * Set Selected Date Rage
-     * @param event
-     */
-    public setDateRange(event) {
-        this.selectedRangeValue = event;
-        this.params = {
-            ...this.params,
-            ...this.formatDateRange(),
-        };
         this.getFeatures();
     }
 
