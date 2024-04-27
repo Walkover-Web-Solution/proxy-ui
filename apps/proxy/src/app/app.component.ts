a44fffcb35f66b0b1b8a3e3bac522e0eb293023a
Rahul Choudhary <rahulchoudhary@walkover.in>
2024-04-27 13:00:52 +0530

    added interface script

--- apps/proxy/src/app/app.component.ts
+++ apps/proxy/src/app/app.component.ts
@@ -3,15 +3,18 @@ import { ActivatedRoute, NavigationEnd, NavigationStart, Router } from '@angular
 import { VersionCheckService } from '@proxy/service';
 import { select, Store } from '@ngrx/store';
 import { isEqual } from 'lodash-es';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
 import { distinctUntilChanged, filter, map, mergeMap, takeUntil } from 'rxjs/operators';
 import { environment } from '../environments/environment';
 import { BaseComponent } from '@proxy/ui/base-component';
-import { selectLogOutSuccess } from './auth/ngrx/selector/login.selector';
+import { selectLogInData, selectLogOutSuccess } from './auth/ngrx/selector/login.selector';
 import { ILogInFeatureStateWithRootState } from './auth/ngrx/store/login.state';
-import { IAppState } from './ngrx';
+import { IAppState, selectClientSettings } from './ngrx';
 import { rootActions } from './ngrx/actions';
 import * as logInActions from './auth/ngrx/actions/login.action';
+import { generateJwtToken } from '@proxy/utils';
+import { IClientSettings, IFirebaseUserModel } from '@proxy/models/root-models';
+import { AuthService } from '@proxy/services/proxy/auth';
 
 @Component({
     selector: 'proxy-root',
@@ -21,6 +24,8 @@ export class AppComponent extends BaseComponent implements OnInit, OnDestroy {
     public title = 'proxy';
     public logoutActionComplete$: Observable<boolean>;
     public companyId$: Observable<string>;
+    public logInData$: Observable<IFirebaseUserModel>;
+    public clientSettings$: Observable<IClientSettings>;
 
     /** True, if new build is deployed */
     private newVersionAvailableForWebApp: boolean = false;
@@ -30,7 +35,8 @@ export class AppComponent extends BaseComponent implements OnInit, OnDestroy {
         private router: Router,
         private actRoute: ActivatedRoute,
         private store: Store<IAppState>,
-        private versionCheckService: VersionCheckService
+        private versionCheckService: VersionCheckService,
+        private authService: AuthService
     ) {
         super();
 
@@ -66,6 +72,16 @@ export class AppComponent extends BaseComponent implements OnInit, OnDestroy {
             distinctUntilChanged(isEqual),
             takeUntil(this.destroy$)
         );
+        this.logInData$ = this._store.pipe(
+            select(selectLogInData),
+            distinctUntilChanged(isEqual),
+            takeUntil(this.destroy$)
+        );
+        this.clientSettings$ = this.store.pipe(
+            select(selectClientSettings),
+            distinctUntilChanged(isEqual),
+            takeUntil(this.destroy$)
+        );
     }
 
     public ngOnInit(): void {
@@ -84,6 +100,43 @@ export class AppComponent extends BaseComponent implements OnInit, OnDestroy {
                 }
             });
         }
+        combineLatest([this.logInData$, this.clientSettings$]).subscribe(([loginData, clientSettings]) => {
+            if (loginData && clientSettings) {
+                const scriptId = 'interface-main-script';
+                if (document.getElementById(scriptId)) {
+                    document.getElementById(scriptId)?.remove();
+                }
+                const scriptElement = document.createElement('script');
+                scriptElement.type = 'text/javascript';
+                scriptElement.src = environment.interfaceScriptUrl;
+                scriptElement.id = scriptId;
+                scriptElement.setAttribute(
+                    'embedToken',
+                    generateJwtToken(
+                        {
+                            org_id: +environment.interfaceOrgId,
+                            project_id: environment.interfaceProjectId,
+                            interface_id: environment.interfaceId,
+                            user_id: loginData?.email,
+                        },
+                        environment.interfaceSecret
+                    )
+                );
+                scriptElement.onload = () => {
+                    (window as any).SendDataToInterface({
+                        variables: {
+                            variables: JSON.stringify({
+                                session: this.authService.getTokenSync(),
+                                project_id: environment.interfaceProjectId,
+                            }),
+                        },
+                        threadId: `${loginData?.email}${clientSettings?.client?.id}`,
+                        bridgeName: 'root',
+                    });
+                };
+                document.body.appendChild(scriptElement);
+            }
+        });
     }
 
     public ngOnDestroy(): void {
