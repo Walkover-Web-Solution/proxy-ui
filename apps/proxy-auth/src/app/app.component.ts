8c695b21cadfc5ed6591cf5e688ae9d962d03661
aliasgar-walker <aliasgar@whozzat.com>
2025-03-17 12:12:43 +0530

    fix

--- apps/proxy-auth/src/app/app.component.ts
+++ apps/proxy-auth/src/app/app.component.ts
@@ -26,8 +26,7 @@ export class AppComponent extends BaseComponent implements OnInit, OnDestroy {
     public initOtpProvider() {
         if (!environment.production) {
             const sendOTPConfig = {
-                authToken:
-                    'ZlRVek91eHhINmtibG5DV1pFMmxlSGY5dmhqVlAyQmc5TFBYU0xLNWQrWFdISk1aTjM1b0JmVjBFSjlBY0NsVk8vMWhhNVJRU3NaaXRzdUh2STcxdjc4cXhxSTNoVHBLNDd3SkJFUktpODVKcEpXQXdOa2lWM3hJbjNGQ2VjU3NzTkh0WUJhNXhnam9pYjZ6enIwaUdtalVxWitZemRHVW8zRE5lUytnUWpzPQ==',
+                referenceId: '1690999i174022063967b9a8dfbdc44',
                 target: '_self',
                 success: (data) => {
                     console.log('success response', data);
