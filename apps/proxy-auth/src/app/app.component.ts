ab59c75ead48ace08bf50576d9e3f2849fad15cb
prakharlowanshi <walkover@walkovers-Mac-mini.local>
2025-09-08 12:39:31 +0530

    fix reference id

--- apps/proxy-auth/src/app/app.component.ts
+++ apps/proxy-auth/src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent extends BaseComponent implements OnInit, OnDestroy {
     public initOtpProvider() {
         if (!environment.production) {
             const sendOTPConfig = {
-                referenceId: '1362783c1752920869687b7325adbf8',
+                referenceId: '1690999i174022063967b9a8dfbdc44',
                 target: '_self',
                 success: (data) => {
                     console.log('success response', data);
