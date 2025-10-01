f733cd88fb9087d5806f2887d469da224d0a9b7b
nisha-walkover <nisha@walkover.in>
2025-10-01 12:10:48 +0530

    lago subscription design fixes

--- apps/proxy-auth/src/app/app.component.ts
+++ apps/proxy-auth/src/app/app.component.ts
@@ -26,7 +26,10 @@ export class AppComponent extends BaseComponent implements OnInit, OnDestroy {
     public initOtpProvider() {
         if (!environment.production) {
             const sendOTPConfig = {
-                referenceId: '1690999i174022063967b9a8dfbdc44',
+                // referenceId: '4512365j175921585768db80f196ea2',
+                userToken:
+                    'eVFZZXBpRVlKOGQranNWYU5CV1NBRjFZcGdPSVJrUE9DSlZ1clhGNjlRa1lMNC9WT2tGSXdkZ3MzbFRYenk4aDdPRUdGem13a0dkVUU2ZitRQU04c2JZRC9pNDRuWHFnSHhuQm5wQUhXWVIxRFF2WExQa3I4Zk5SV2QrdTB3aDlZVHU2MWc2VFRjVUNWL1Qrb3NrSmhvZ205VmhTSDBGbzVCaDZhQTBYS3pQbUdScEkySkV6bVVMNFJHR3VqaExwWUt2aDRXRENtQXlRZ2xYYm5jU0VFdz09',
+                pass: 'dfjk',
                 target: '_self',
                 success: (data) => {
                     console.log('success response', data);
