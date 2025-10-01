5798d6d083e4a554815302d9871a7a8b18133c72
prakharlowanshi <walkover@walkovers-Mini.localdomain>
2025-10-01 18:06:52 +0530

    remove usertoken

--- apps/proxy-auth/src/app/app.component.ts
+++ apps/proxy-auth/src/app/app.component.ts
@@ -26,10 +26,7 @@ export class AppComponent extends BaseComponent implements OnInit, OnDestroy {
     public initOtpProvider() {
         if (!environment.production) {
             const sendOTPConfig = {
-                // referenceId: '4512365j175921585768db80f196ea2',
-                userToken:
-                    'eVFZZXBpRVlKOGQranNWYU5CV1NBRjFZcGdPSVJrUE9DSlZ1clhGNjlRa1lMNC9WT2tGSXdkZ3MzbFRYenk4aDdPRUdGem13a0dkVUU2ZitRQU04c2JZRC9pNDRuWHFnSHhuQm5wQUhXWVIxRFF2WExQa3I4Zk5SV2QrdTB3aDlZVHU2MWc2VFRjVUNWL1Qrb3NrSmhvZ205VmhTSDBGbzVCaDZhQTBYS3pQbUdScEkySkV6bVVMNFJHR3VqaExwWUt2aDRXRENtQXlRZ2xYYm5jU0VFdz09',
-                pass: 'dfjk',
+                referenceId: '1690999i174022063967b9a8dfbdc44',
                 target: '_self',
                 success: (data) => {
                     console.log('success response', data);
