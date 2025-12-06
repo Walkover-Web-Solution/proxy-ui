a5527f7c48d18eb91c43cee5a0f0e5bacd36bf49
prakharlowanshi11 <prakhar@whozzat.com>
2025-12-06 16:05:52 +0530

    Revert "P0051 | Preview of the authentication"

--- apps/proxy/src/app/features/create-feature/create-feature.component.ts
+++ apps/proxy/src/app/features/create-feature/create-feature.component.ts
@@ -130,7 +130,6 @@ export class CreateFeatureComponent extends BaseComponent implements OnDestroy, 
     public updatePaymentDetails$: Observable<any> = this.componentStore.updatePaymentDetails$;
 
     public isEditMode = false;
-    public previewInputPosition: 'top' | 'bottom' = 'top';
     public selectedServiceIndex = 0;
     public selectedSubscriptionServiceIndex = -2;
 
